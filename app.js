f31e586ef587497345497a9bf1432a89813c919a
bettyfileti <beth@politetype.com>
2022-04-16 15:50:49 -0400

    working on initial structure and interactions

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 let flagCount = 0;
 let flagCountDisplay = document.getElementById("flag-count");
+
 let btn_makeAFlag = document.getElementById("make-a-flag");
 let btn_makeAFlagMachine = document.getElementById("make-a-flag-machine");
+let nextInstruction = document.getElementsByClassName("next-instruction")[0];
 
-let flagString = "[&ensp;]";
+let flagString = "&ensp;[&ensp;]&ensp;";
 let flagWarehouse = document.getElementById("flag-warehouse");
 
 btn_makeAFlag.addEventListener("click", makeAFlag);
@@ -28,8 +30,19 @@ function makeAFlagMachine(){
 
 function putFlagInWarehouse(){
     let newFlag = document.createElement("pre");
-    newFlag.ariaLabel = "1 flag"
-    newFlag.class = "ascii-art"
+    newFlag.ariaLabel = "1 flag";
+    newFlag.class = "ascii-art";
     newFlag.innerHTML = flagString;
     flagWarehouse.append(newFlag);
+    flagCountIncreased();
 }
+
+function flagCountIncreased(){
+    if (flagCount == 6){
+        nextInstruction.style.display = "inline";
+    } else if (flagCount == 11){
+        nextInstruction.innerHTML = "Let's try a machine."
+        btn_makeAFlagMachine.style.display = "inline";
+    }
+}
+
