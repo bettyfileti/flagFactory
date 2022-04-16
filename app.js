e9da60c94e3d57ddef2f45fc1406faa3debfe8c0
bettyfileti <beth@politetype.com>
2022-04-16 17:07:58 -0400

    Added ability to sell flag

--- app.js
+++ app.js
@@ -1,31 +1,41 @@
 let flagCount = 0;
-let flagCountDisplay = document.getElementById("flag-count");
+let flagsMade = 0;
+let flagsSold = 0;
+let flagsMadeDisplay = document.getElementById("flags-made-count");
 
 let btn_makeAFlag = document.getElementById("make-a-flag");
 let btn_makeAFlagMachine = document.getElementById("make-a-flag-machine");
+let btn_sellAFlag = document.getElementById("sell-a-flag");
 let nextInstruction = document.getElementsByClassName("next-instruction")[0];
+let flagMachineOn = false;
+let flagMachineStartTime;
 
 let flagString = "&ensp;[&ensp;]&ensp;";
 let flagWarehouse = document.getElementById("flag-warehouse");
 
 btn_makeAFlag.addEventListener("click", makeAFlag);
 btn_makeAFlagMachine.addEventListener("click", makeAFlagMachine);
+btn_sellAFlag.addEventListener("click", sellAFlag);
 
 function makeAFlag() {
-    flagCount += 1;
-    flagCountDisplay.innerHTML = flagCount;
-    document.getElementsByClassName("flag-count-container")[0].style.display = "flex";
+    flagsMade += 1;
+    flagsMadeDisplay.innerHTML = flagsMade;
+    document.getElementsByClassName("flags-made-container")[0].style.display = "flex";
     putFlagInWarehouse();
 }
 
 function makeAFlagMachine(){
-    console.log("Making a Flag Machine")
+    console.log("Making a Flag Machine");
+    flagMachineOn = true;
+    flagMachineStartTime = flagsMade;
     setInterval(function(){ 
         //this code runs every second 
-        flagCount += 1;
-        flagCountDisplay.innerHTML = flagCount;
+        flagsMade += 1;
+        flagsMadeDisplay.innerHTML = flagsMade;
         putFlagInWarehouse();
     }, 1000);
+    nextInstruction.innerHTML = "That is so much better.";
+    btn_makeAFlagMachine.disabled = "true";
 }
 
 function putFlagInWarehouse(){
@@ -34,15 +44,37 @@ function putFlagInWarehouse(){
     newFlag.class = "ascii-art";
     newFlag.innerHTML = flagString;
     flagWarehouse.append(newFlag);
-    flagCountIncreased();
+    flagsMadeIncreased();
 }
 
-function flagCountIncreased(){
-    if (flagCount == 6){
+function flagsMadeIncreased(){
+    if (flagsMade == 6){
         nextInstruction.style.display = "inline";
-    } else if (flagCount == 11){
-        nextInstruction.innerHTML = "Let's try a machine."
-        btn_makeAFlagMachine.style.display = "inline";
+    } else if (flagsMade == 8){
+        nextInstruction.innerHTML = "Maybe we can automate this?";
+    } else if (flagsMade == 11){
+        nextInstruction.innerHTML = "I know. Let's try a machine.";
+        btn_makeAFlagMachine.className = "active";
+    } else if (flagMachineOn){       
+        if ((flagsMade - flagMachineStartTime) > 5){
+            nextInstruction.innerHTML = "The warehouse is getting a little crowded."
+        } 
+        if ((flagsMade - flagMachineStartTime) > 10){
+            nextInstruction.innerHTML = "Maybe we can sell some?"
+            document.getElementById("flags-sold").style.display = "inline";
+            btn_sellAFlag.className = "active";
+        }
     }
 }
 
+function sellAFlag(){
+    console.log("Selling a flag");
+    flagsSold += 1;
+    document.getElementById("flags-sold-count").innerHTML = flagsSold;
+    removeFlagFromWarehouse();
+}
+
+function removeFlagFromWarehouse(){
+    flagWarehouse.removeChild(flagWarehouse.lastChild);
+    flagCount = flagCount - 1;
+}
